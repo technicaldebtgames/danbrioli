3e5aaa45ec8a494d8c2e0a4802688a45764fd6bf
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-12 01:20:54 -0400

    slight textual changes, added parallax background image functionality

---
+++ logic/header_image_scroll.js
@@ -0,0 +1,10 @@
+document.getElementById("body").onscroll = function parallax() {
+
+    var scrollTop = document.scrollingElement.scrollTop;
+    console.log(scrollTop);
+    var target = document.getElementById("header");
+    var x = "center";
+    var y = scrollTop * 0.45;
+    target.style.backgroundPosition = x + " " + y + "px";
+
+}
