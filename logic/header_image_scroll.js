9207fec16be4630ebeb41ae3af0029838bb6c676
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-12 07:36:38 -0400

    junk

--- logic/header_image_scroll.js
+++ logic/header_image_scroll.js
@@ -2,6 +2,6 @@ document.getElementById("body").onscroll = function parallax() {
 
     var scrollTop = document.scrollingElement.scrollTop;
     var target = document.getElementById("header");
-    target.style.backgroundPosition = "center " + (scrollTop * 0.375) + "px";
+    target.style.backgroundPosition = "center " + (scrollTop * 0.3) + "px";
 
 }
