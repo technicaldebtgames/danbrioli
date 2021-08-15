97d2c8efff7229d8bffe7cd94029747b5183d45d
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-15 07:35:04 -0400

    continued

--- logic/header_image_scroll.js
+++ logic/header_image_scroll.js
@@ -1,8 +1,8 @@
 if (!window.has_run_root_modification) {
 
     window.has_run_root_modification = true;
-    var root = document.querySelector(":root");
-    root.setAttribute("style", "font-size: 62.5%; background-color: #784421; background-position: center 0; background-size: 100% auto; background-repeat: no-repeat;");
+    var root = document.querySelector("html");
+    root.setAttribute("style", "font-size: 62.5%; background-color: #784421; background-position: center 0; background-size: 100% auto; background-attachment: scroll; background-repeat: no-repeat;");
 
 }
 
