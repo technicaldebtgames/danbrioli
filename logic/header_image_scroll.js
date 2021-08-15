7af3a56479a744b4d530c3ceac2b25c2c79aabed
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-15 08:03:07 -0400

    continued

--- logic/header_image_scroll.js
+++ logic/header_image_scroll.js
@@ -1,17 +1,27 @@
-if (!window.has_run_root_modification) {
+/*if (!window.has_run_root_modification) {
 
     window.has_run_root_modification = true;
     var root = document.querySelector("html");
     root.setAttribute("style", "font-size: 62.5%; background-color: #784421; background-position: center 0; background-size: 100% auto; background-attachment: scroll; background-repeat: no-repeat;");
 
-}
+}*/
+
+/*document.getElementById("body").onscroll = function parallax() {
+
+    var scrollTop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("banner-image");
+    var value = scrollTop * 0.7;
+    var newPosition = "center " + value + "px";
+    target.style.backgroundPosition = newPosition;
+
+}*/
 
 document.getElementById("body").onscroll = function parallax() {
 
     var scrollTop = document.scrollingElement.scrollTop;
-    var target = document.querySelector(":root");
-    var value = scrollTop * 0.7;
-    var newPosition = "center " + value + "px";
-    target.style.backgroundPosition = newPosition;
+    var target = document.getElementById("banner-image");
+    var value = scrollTop * 0.65;
+    var newTop = value + "px";
+    target.style.top = newTop;
 
 }
