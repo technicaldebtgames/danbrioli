f23846b607ada62dce986688a49ded1d683c5de5
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-15 05:56:35 -0400

    script adjust to test mobile bug

--- logic/header_image_scroll.js
+++ logic/header_image_scroll.js
@@ -2,6 +2,8 @@ document.getElementById("body").onscroll = function parallax() {
 
     var scrollTop = document.scrollingElement.scrollTop;
     var target = document.getElementById("header");
-    target.style.backgroundPosition = "center " + (scrollTop * 0.3) + "px";
+    var value = scrollTop * 0.3;
+    var newPosition = "center " + value + "px";
+    target.style.backgroundPosition = newPosition;
 
 }
