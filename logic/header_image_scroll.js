140b3cfcb009567f7b4c87eb164deac17e5775b5
Dan Brioli <technicaldebtgames@gmail.com>
2021-08-15 07:29:14 -0400

    bug test continues, js modifications

--- logic/header_image_scroll.js
+++ logic/header_image_scroll.js
@@ -1,3 +1,11 @@
+if (!window.has_run_root_modification) {
+
+    window.has_run_root_modification = true;
+    var root = document.querySelector(":root");
+    root.setAttribute("style", "font-size: 62.5%; background-color: #784421; background-position: center 0; background-size: 100% auto; background-repeat: no-repeat;");
+
+}
+
 document.getElementById("body").onscroll = function parallax() {
 
     var scrollTop = document.scrollingElement.scrollTop;
