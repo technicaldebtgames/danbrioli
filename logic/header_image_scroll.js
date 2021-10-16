15e5a43a20bdecd925cf582151f5ac54af4f09cc
Dan Brioli <technicaldebtgames@gmail.com>
2021-10-16 19:12:35 -0400

    site redo, super simple, added some medium series links

--- logic/header_image_scroll.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("body").onscroll = function parallax() {
-
-    var scrollTop = document.scrollingElement.scrollTop;
-    var target = document.getElementById("banner-image-container");
-    var value = scrollTop * 0.65;
-    var newTop = value + "px";
-    target.style.top = newTop;
-
-}
